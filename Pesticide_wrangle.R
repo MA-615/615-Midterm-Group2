9aed418db2f9c894f2df6ba73dcd62cb3e77616e
Lauren Temple <89480531+ltemps@users.noreply.github.com>
2021-10-25 18:21:59 -0400

    fixed comments to match new tidy data

--- Pesticide_wrangle.R
+++ Pesticide_wrangle.R
@@ -1,13 +1,14 @@
 #Pesticide Wrangle
-library(dplyr)
-library(tidyverse)
+
+pacman::p_load('dplyr', 'tidyverse')
+
 Pesticides <- read_csv("Pesticides.csv")
 
-#carcinogen = known or possible or probable
-## hormone disruptor = suspected
-## neurotoxins = present or NA
-## developmental or reproductive toxins = present
-## bee toxins = slight or high or moderate
+# carcinogen = known or possible or probable
+# hormone disruptor = suspected
+# bee toxins = high
+
+#Tidy table
 Pesti <- Pesticides %>%
   filter(Carcinogen == "known" | Carcinogen == "possible" | Carcinogen == "probable") %>%
   filter(`Hormone Disruptor` =="suspected") %>%
