9d974eece8cf573786e4bf4aedddf048f2f6a53e
Lauren Temple <89480531+ltemps@users.noreply.github.com>
2021-10-25 18:18:32 -0400

    added bee toxin=high and condensed code to produce one tidy table

--- Pesticide_wrangle.R
+++ Pesticide_wrangle.R
@@ -8,7 +8,7 @@ Pesticides <- read_csv("Pesticides.csv")
 ## neurotoxins = present or NA
 ## developmental or reproductive toxins = present
 ## bee toxins = slight or high or moderate
-pesti1 <- Pesticides %>%
-  filter(Carcinogen == "known" | Carcinogen == "possible" | Carcinogen == "probable")
-pesti2 <- pesti1 %>%
-  filter(`Hormone Disruptor` =="suspected" & `Developmental or Reproductive Toxins`=="present")
+Pesti <- Pesticides %>%
+  filter(Carcinogen == "known" | Carcinogen == "possible" | Carcinogen == "probable") %>%
+  filter(`Hormone Disruptor` =="suspected") %>%
+  filter(`Bee Toxins`=="high")
