66996201e8a0d548ddf8cc6829fd4b0cb2d3c077
lst <lst@bu.edu>
2021-10-25 15:40:00 -0400

    filter Pesticides based on conditions

--- Pesticide_wrangle.R
+++ Pesticide_wrangle.R
@@ -1,1 +1,14 @@
-Pesticide Wrangle
+#Pesticide Wrangle
+library(dplyr)
+library(tidyverse)
+Pesticides <- read_csv("Pesticides.csv")
+
+#carcinogen = known or possible or probable
+## hormone disruptor = suspected
+## neurotoxins = present or NA
+## developmental or reproductive toxins = present
+## bee toxins = slight or high or moderate
+pesti1 <- Pesticides %>%
+  filter(Carcinogen == "known" | Carcinogen == "possible" | Carcinogen == "probable")
+pesti2 <- pesti1 %>%
+  filter(`Hormone Disruptor` =="suspected" & `Developmental or Reproductive Toxins`=="present")
