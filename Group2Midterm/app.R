e4000a0b30397049d60cf52cf6cae3e0c5d8509a
Zening Ye <znye@bu.edu>
2021-10-31 21:47:17 -0400

    V1 Shiny app ZY Oct. 31, 2021

--- Group2Midterm/app.R
+++ Group2Midterm/app.R
@@ -3,41 +3,43 @@ library(tidyverse)
 library(ggplot2)
 
 # what pesticides do each state use and how toxic are they?
-df <- read.csv("data/final.csv")
-
-states <- c("NEW YORK", "WASHINGTON", "OREGON", "NORTH CAROLINA", "CALIFORNIA", "FLORIDA")
+states <- c("WASHINGTON", "OREGON", "CALIFORNIA", "FLORIDA")
 
 
 # UI 
 ui <- fluidPage(
-  selectInput("state", "Please Select the State", states),
-  tableOutput("table")
+  
+  # Indicate the State
+  selectInput(inputId = "state", label = "Please Select the State", states),
+  
+  # Setup table output
+  tableOutput('table')
 )
 
 
 # Server
 # Server function has some problem and I did not fix it. Hopefully someone can help.
 server <- function(input, output, session) {
-  input$state <- renderText(input$state)
-  if(input$state == "CALIFORNIA") {
-    df1 <- df %>% filter(df$State == "CALIFORNIA")
-    output$table <- renderTable(df1)
-  } else if (input$state == "NEW YORK") {
-    df2 <- df %>% filter(df$State == "NEW YORK")
-    output$table <- renderTable(df2) 
-  } else if (input$state == "WASHINGTON") {
-    df3 <- df %>% filter(df$State == "WASHINGTON")
-    output$table <- renderTable(df3) 
-  } else if (input$state == "OREGON") {
-    df4 <- df %>% filter(df$State == "OREGON")
-    output$table <- renderTable(df4) 
-  } else if (input$state == "NORTH CAROLINA") {
-    df5 <- df %>% filter(df$State == "NORTH CAROLINA")
-    output$table <- renderTable(df5) 
-  } else if (input$state == "FLORIDA") {
-    df6 <- df %>% filter(df$State == "FLORIDA")
-    output$table <- renderTable(df6) 
-  } else stop("Warning")
+ 
+  # Data output in table formate
+  output$table <- renderTable(
+   if (input$state == "CALIFORNIA") {
+     df1 <- strawbPesti %>% filter(strawbPesti$State == input$state)
+     return(df1)
+   } else if (input$state == "WASHINGTON") {
+     df2 <- strawbPesti %>% filter(strawbPesti$State == input$state)
+     return(df2)
+   } else if (input$state == "OREGON") {
+     df3 <- strawbPesti %>% filter(strawbPesti$State == input$state)
+     return(df3)
+   } else {
+     df4 <- strawbPesti %>% filter(strawbPesti$State == input$state)
+     return(df4)
+   }
+ )
+ 
+ 
+ 
 }
 
 shinyApp(ui, server)
