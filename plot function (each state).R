7bd741b0e5b8d7c2b3e0a73b90d39d2f24b1f328
lst <lst@bu.edu>
2021-11-01 21:20:20 -0400

    upload plot function for shiny source ST

---
+++ plot function (each state).R
@@ -0,0 +1,29 @@
+## data
+strawbPesti <-read.csv("https://raw.githubusercontent.com/MA-615/615-Midterm-Group2/main/data/strawbPesti.csv")
+strawbPesti <- arrange(strawbPesti,`Chemical Name`)
+strawbPesti1 <- strawbPesti[,-13]
+strawbPesti2 <- strawbPesti1[,c(2,4,7,12,13,20)]
+strawbPesti3 <- filter(strawbPesti2, Discription==" MEASURED IN LB")
+strawbPesti4 <- filter(strawbPesti3, Value !="(D)")
+strawbPesti4$Value <- as.numeric(sub(",", "", strawbPesti4$Value, fixed = TRUE))
+
+strawbPestiAVG <- strawbPesti4 %>%
+  group_by(State, `Chemical Name`,`Human Toxins`) %>%
+  summarise(Value,Value=mean(Value))
+
+
+
+## function
+pestiToxin <- function(state1){
+  data <- filter(strawbPestiAVG,State==state1)
+  bar <- ggplot(data)+
+    geom_bar(mapping = aes(x=`Chemical Name`,y=Value,fill=`Human Toxins`), position = "dodge",stat = "identity")+
+    scale_fill_manual(values = c("high" = "#D55E00", "moderate"="#E69F00", "slight"="#009E73"))+
+    labs(title = state1)+
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+  bar2 <- ggplot(data)+
+    geom_bar(mapping = aes(x=`Chemical Name`,y=Value,fill=`Human Toxins`), position = "dodge",stat = "identity")+
+    scale_fill_manual(values = c("high" = "#D55E00", "moderate"="#E69F00", "slight"="#009E73"))+
+    labs(title = state1)
+  list(bar,ggplotly(bar), bar2+coord_polar())
+}
